96b5028a688610b053d9296f4ad9750b58c724f1
jangsehui <hee990605@naver.com>
2023-02-19 17:20:12 +0900

    movie page style

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import axios from "axios";
 import React from "react";
-import Movie from "./Movie";
+import Home from "./routes/Home";
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: {movies}
-      }
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({movies, isLoading: false});
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
-          </div>
-        ):(
-          <div class="movies">
-            {movies.map(movie => (
-                <Movie 
-                          key={movie.id}
-                          id={movie.id}
-                          year={movie.year}
-                          title={movie.title}
-                          summary={movie.summary}
-                          poster={movies.mideum_cover_image}
-                          />
-              
-            ))}
-          </div>
-        )}
-      </section>
-    )
-    
-  }
+function App() {
+  return (<Home />);
 }
 
 export default App;
